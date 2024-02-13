33d63d2c30edd80f9e04d23a3ce812abca92fa1c
Limoneto <84030325+Limoneto@users.noreply.github.com>
2024-02-13 18:34:26 -0300

    deployment

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"gdDaOsrkoB3jKGxitaZB+DJSma7t3hMR+NHS+jdO9gQ=\"\n}"
---
+++ .next/static/webpack/app/dashboard/layout.20b8729a10c6b9b5.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/dashboard/layout",{
+
+/***/ "(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=D%3A%5CPrograming%5Cnext%5Cnextjs-dashboard%5Capp%5Cui%5Cdashboard%5Cnav-links.tsx&modules=D%3A%5CPrograming%5Cnext%5Cnextjs-dashboard%5Cnode_modules%5Cnext%5Cdist%5Cclient%5Clink.js&server=false!":
+/*!******************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=D%3A%5CPrograming%5Cnext%5Cnextjs-dashboard%5Capp%5Cui%5Cdashboard%5Cnav-links.tsx&modules=D%3A%5CPrograming%5Cnext%5Cnextjs-dashboard%5Cnode_modules%5Cnext%5Cdist%5Cclient%5Clink.js&server=false! ***!
+  \******************************************************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./app/ui/dashboard/nav-links.tsx */ \"(app-pages-browser)/./app/ui/dashboard/nav-links.tsx\"));\nPromise.resolve(/*! import() eager */).then(__webpack_require__.t.bind(__webpack_require__, /*! ./node_modules/next/dist/client/link.js */ \"(app-pages-browser)/./node_modules/next/dist/client/link.js\", 23))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL25vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvYnVpbGQvd2VicGFjay9sb2FkZXJzL25leHQtZmxpZ2h0LWNsaWVudC1lbnRyeS1sb2FkZXIuanM/bW9kdWxlcz1EJTNBJTVDUHJvZ3JhbWluZyU1Q25leHQlNUNuZXh0anMtZGFzaGJvYXJkJTVDYXBwJTVDdWklNUNkYXNoYm9hcmQlNUNuYXYtbGlua3MudHN4Jm1vZHVsZXM9RCUzQSU1Q1Byb2dyYW1pbmclNUNuZXh0JTVDbmV4dGpzLWRhc2hib2FyZCU1Q25vZGVfbW9kdWxlcyU1Q25leHQlNUNkaXN0JTVDY2xpZW50JTVDbGluay5qcyZzZXJ2ZXI9ZmFsc2UhIiwibWFwcGluZ3MiOiJBQUFBLDBMQUE4RztBQUM5RyIsInNvdXJjZXMiOlsid2VicGFjazovL19OX0UvPzYzZDciXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0KC8qIHdlYnBhY2tNb2RlOiBcImVhZ2VyXCIgKi8gXCJEOlxcXFxQcm9ncmFtaW5nXFxcXG5leHRcXFxcbmV4dGpzLWRhc2hib2FyZFxcXFxhcHBcXFxcdWlcXFxcZGFzaGJvYXJkXFxcXG5hdi1saW5rcy50c3hcIik7XG5pbXBvcnQoLyogd2VicGFja01vZGU6IFwiZWFnZXJcIiAqLyBcIkQ6XFxcXFByb2dyYW1pbmdcXFxcbmV4dFxcXFxuZXh0anMtZGFzaGJvYXJkXFxcXG5vZGVfbW9kdWxlc1xcXFxuZXh0XFxcXGRpc3RcXFxcY2xpZW50XFxcXGxpbmsuanNcIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=D%3A%5CPrograming%5Cnext%5Cnextjs-dashboard%5Capp%5Cui%5Cdashboard%5Cnav-links.tsx&modules=D%3A%5CPrograming%5Cnext%5Cnextjs-dashboard%5Cnode_modules%5Cnext%5Cdist%5Cclient%5Clink.js&server=false!\n"));
+
+/***/ })
+
+});
